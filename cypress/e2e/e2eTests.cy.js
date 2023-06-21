8bbd0c43e3a67d8f6e00c15c961d13bb5953a707
Tim Douglas <timmyddthe1st@Tims-MacBook-Air.local>
2023-06-21 21:17:27 +0100

    added an e2e test

--- cypress/e2e/e2eTests.cy.js
+++ cypress/e2e/e2eTests.cy.js
@@ -1,11 +1,12 @@
 /// <reference types="cypress" />
 
-describe('Currency conversion', () => {
+describe('Empty author state', () => {
   beforeEach(() => {
     cy.visit('localhost:3000')
   })
 
   it('displays as expected with default values', () => {
-
+    cy.contains('Github Repository Viewer')
+    cy.contains('No Repositories Found')
   })
 })
